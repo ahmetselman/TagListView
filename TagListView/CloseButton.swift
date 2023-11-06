0005ab557f2793cb0e350235c9d8d72bdd8c73c2
Ahmet Selman Durak <adurak@thinksmobility.com>
2023-11-06 21:34:18 +0300

    change line width of x

--- TagListView/CloseButton.swift
+++ TagListView/CloseButton.swift
@@ -11,7 +11,7 @@ import UIKit
 internal class CloseButton: UIButton {
 
     var iconSize: CGFloat = 10 // İkon boyutu olarak kullanılacak
-    var lineWidth: CGFloat = 1
+    var lineWidth: CGFloat = 2
     var lineColor: UIColor = .white // "X" işareti için beyaz renk
     var circleColor: UIColor = UIColor(red: 12/255, green: 33/255, blue: 58/255, alpha: 1) // Arka plan daire rengi
 
@@ -60,40 +60,3 @@ internal class CloseButton: UIButton {
         }
     }
 }
-
-/*
-import UIKit
-
-internal class CloseButton: UIButton {
-
-    var iconSize: CGFloat = 10
-    var lineWidth: CGFloat = 1
-    var lineColor: UIColor = UIColor.white.withAlphaComponent(0.54)
-
-    weak var tagView: TagView?
-
-    override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
-
-        path.lineWidth = lineWidth
-        path.lineCapStyle = .round
-
-        let iconFrame = CGRect(
-            x: (rect.width - iconSize) / 2.0,
-            y: (rect.height - iconSize) / 2.0,
-            width: iconSize,
-            height: iconSize
-        )
-
-        path.move(to: iconFrame.origin)
-        path.addLine(to: CGPoint(x: iconFrame.maxX, y: iconFrame.maxY))
-        path.move(to: CGPoint(x: iconFrame.maxX, y: iconFrame.minY))
-        path.addLine(to: CGPoint(x: iconFrame.minX, y: iconFrame.maxY))
-
-        lineColor.setStroke()
-
-        path.stroke()
-    }
-
-}
-*/
